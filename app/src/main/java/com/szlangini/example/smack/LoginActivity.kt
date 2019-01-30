0d0ee17215273c717bb1c924841c42c2ddb5e734
szlangini <vincent.szlang@googlemail.com>
2019-01-30 17:40:37 +0100

    Layout for Login UI added, Intent from Nav to ActivityLogin

---
+++ app/src/main/java/com/szlangini/example/smack/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.szlangini.example.smack
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun loginLoginButtonClicked(view : View) {}
+    fun loginCreateUserButtonClicked(view :View) {}
+}
--- app/src/main/java/com/szlangini/example/smack/MainActivity.kt
+++ app/src/main/java/com/szlangini/example/smack/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.szlangini.example.smack
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
@@ -36,12 +37,13 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun loginBtnNavHeader(view: View) {
-
+    fun loginBtnNavClicked(view: View) {
+        val loginIntent = Intent(this, LoginActivity::class.java)
+        startActivity(loginIntent)
     }
 
     fun addChannelBtnClicked(view: View){
-        
+
     }
 
     fun sendMessageBtn(view: View){
